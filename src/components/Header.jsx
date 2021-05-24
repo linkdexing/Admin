df97ad8bbdd42a7235c3b93122a083bc733c002c
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-05-25 00:29:33 +0530

    search
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { privateApi } from "../api";
+import { userUrl } from "../api/endpoints";
 
 const Header = () => {
+  const { handleSubmit } = useForm();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let { users } = (await privateApi.get(`${userUrl}/search`)).data;
+      setUsers(users);
+    };
+    fetchData();
+  }, []);
+
+  const onSubmit = async (values) => {
+    try {
+      axios.get(`${userUrl}/search`, function (req, res, next) {
+        var q = req.query.q;
+        setUsers
+          .find(
+            {
+              email: {
+                $regex: new RegExp(q),
+              },
+            },
+            {
+              _id: 0,
+              __v: 0,
+            },
+            function (err, data) {
+              res.json(data);
+            }
+          )
+          .limit(10);
+      });
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
+
   return (
     <nav
       className="navbar navbar-dark bg-dark navbar-expand-lg"
@@ -32,6 +74,23 @@ const Header = () => {
               <a href="/Manage_users" className="nav-link active">
                 Manage-Users
               </a>
+            </li>
+            <li className="nav-item">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="search"
+                    placeholder="Search User"
+                  />
+                  <input
+                    type="submit"
+                    value="search"
+                    className="btn btn-primary"
+                  />
+                </div>
+              </form>
             </li>
           </ul>
         </div>
