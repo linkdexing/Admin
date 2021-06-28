c41224c536183457643bd96405ce2667e50bc985
Jasmeet.Singh2 <jasmeet.singh2@ibm.com>
2021-06-28 20:35:47 +0530

    change limits modal completed
    Signed-off-by: Jasmeet.Singh2 <jasmeet.singh2@ibm.com>

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { privateApi } from '../api';
-import { userUrl, userVariablesUrl } from '../api/endpoints';
+import { authUrl, userUrl, userVariablesUrl } from '../api/endpoints';
 
 const Users = () => {
   const { register, handleSubmit } = useForm();
   const [users, setUsers] = useState([]);
 
   const [refresh, setRefresh] = useState(true);
+
+  const [currentUserIndex, setCurrentUserIndex] = useState(0);
+
+  const [limitValue, setLimitValue] = useState(7000);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -60,6 +64,30 @@ const Users = () => {
     setRefresh(true);
   };
 
+  const handleOpenModal = (index) => {
+    setCurrentUserIndex(index);
+    setLimitValue(users[index].userVariables.monthlyLimit);
+  };
+
+  const handleChange = (e) => {
+    setLimitValue(e.target.value);
+  };
+
+  const handleEditLimit = async (id) => {
+    try {
+      await privateApi.post(`${authUrl}/change-limit/${id}`, {
+        limit: limitValue,
+      });
+      toast.success('Links limit changed');
+      setRefresh(true);
+    } catch (err) {
+      toast.error(
+        err.response?.data.message ||
+          'Something went wrong, please try again later'
+      );
+    }
+  };
+
   return (
     <div className="container mt-4">
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -90,7 +118,7 @@ const Users = () => {
         </div>
       </form>
       <div>
-        <table className="table table-hover table-responsive table-bordered mt-4">
+        <table className="table table-hover table-responsive table-bordered mt-4 align-middle">
           <thead className="text-center">
             <tr>
               <th scope="col">User Name</th>
@@ -113,14 +141,24 @@ const Users = () => {
               <th></th>
             </tr>
             {React.Children.toArray(
-              users.map((user) => (
+              users.map((user, index) => (
                 <tr
                   className={user.totalLinks >= 10000 ? 'table-danger' : null}
                 >
                   <td>{user.name}</td>
                   <td>{user.email}</td>
 
-                  <td>{user.userVariables.monthlyLimit}</td>
+                  <td className="d-flex align-items-center">
+                    {user.userVariables.monthlyLimit}{' '}
+                    <button
+                      className="ms-auto btn btn-primary"
+                      data-bs-toggle="modal"
+                      data-bs-target="#ModalLinks"
+                      onClick={() => handleOpenModal(index)}
+                    >
+                      Edit
+                    </button>
+                  </td>
                   <td>{user.userVariables.monthlyUsed}</td>
                   <td>{user.userVariables.totalLinks}</td>
 
@@ -158,6 +196,56 @@ const Users = () => {
           </tbody>
         </table>
       </div>
+
+      <div
+        className="modal fade"
+        id="ModalLinks"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Change Links Limit
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <label className="form-label">New Limit</label>
+              <input
+                type="number"
+                className="form-control"
+                value={limitValue}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleEditLimit(users[currentUserIndex]._id)}
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* </Modal> */}
     </div>
   );
 };
